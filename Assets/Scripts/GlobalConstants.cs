5bd12501ff5d479b4d6dabe2efc8e457ba5d20a5
Joel Ng <blood_elves88@yahoo.com.sg>
2014-09-20 16:57:47 +0800

    Disabled god mode, set boss health to 300

--- Assets/Scripts/GlobalConstants.cs
+++ Assets/Scripts/GlobalConstants.cs
@@ -34,7 +34,7 @@ public static class Constants {
 	public const int PISTOL_MAGAZINE_SIZE = 6;			// Amount of shots in 1 pistol magazine
 	
 	// Boss stats	
-	public const float BOSS_TOTAL_HEALTH = 100.0f;				// Amount of health the boss has
+	public const float BOSS_TOTAL_HEALTH = 300.0f;				// Amount of health the boss has
 		// First 20% - shooting rate
 		public const float BOSS_SPRAY_TIME = 3.0f;				// Time spent shooting
 		public const float BOSS_IDLE_TIME = 2.0f;				// Time spent idling
